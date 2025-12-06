cd64d5e19467462575e78fde2ad149e7303aebc9
luisfer1703 <camporico932@gmail.com>
2025-12-06 11:57:03 -0400

    edward

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -7,7 +7,7 @@ function WelcomeMessage() {
     return (
         <div className="w-screen bg-[#FCFCFD] h-screen flex justify-center py-10 px-4">
             <h1 className="text-[#3843D0] text-3x1 mt-10 font-semibold">
-                Bienvenidos chavales!!😲😲😲
+                Bienvenidos chavales!!😲😲😲 xd
             </h1>
         </div>
     );
